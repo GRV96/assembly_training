7096bd1b8c0a842a5dc60a24dc5d54f00cd38d11
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-09-13 11:44:38 -0400

    Section .data can be written before section .text.

--- hello_world.asm
+++ hello_world.asm
@@ -3,18 +3,6 @@
 
 global _start
 
-section .text
-_start:
-	mov eax, 0x4        ; Syscall 4: write
-	mov ebx, 1          ; Argument int fd: file descriptor. 1 is stdout.
-	mov ecx, MESSAGE    ; Argument const void *buf: the string to write
-	mov edx, MSG_LENGTH ; Argument size_t count: number of bytes in buf
-	int 0x80            ; int: interrupt. 0x80: run the syscall in eax.
-
-	mov eax, 0x1 ; Syscall 1: exit
-	mov ebx, 0   ; Return value 0: no error
-	int 0x80
-
 section .data
 	; db: define bytes
 	; 0xA = 10 = '\n'
@@ -26,3 +14,15 @@ section .data
 	; Works only immediately after the string's declaration
 	; Explained in a comment by Patrick Sevallius.
 	MSG_LENGTH: equ $-MESSAGE
+
+section .text
+_start:
+	mov eax, 0x4        ; Syscall 4: write
+	mov ebx, 1          ; Argument int fd: file descriptor. 1 is stdout.
+	mov ecx, MESSAGE    ; Argument const void *buf: the string to write
+	mov edx, MSG_LENGTH ; Argument size_t count: number of bytes in buf
+	int 0x80            ; int: interrupt. 0x80: run the syscall in eax.
+
+	mov eax, 0x1 ; Syscall 1: exit
+	mov ebx, 0   ; Return value 0: no error
+	int 0x80
