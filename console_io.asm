24f8f3715869e1c103cf208c112301d65ef8717a
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-09-18 17:09:57 -0400

    Output message written in the console, unnecessary comments erased

--- console_io.asm
+++ console_io.asm
@@ -3,17 +3,27 @@ global _start
 .data:
 	INPUT_MSG db "Enter a character: "
 	IN_MSG_LENGTH equ $-INPUT_MSG
-	OUTPUT_MSG db "You have entered "
+
+	OUTPUT_MSG db 0xA, "You have entered "
 	OUT_MSG_LENGTH equ $-OUTPUT_MSG
 
 .text:
 _start:
-	mov eax, 4             ; Syscall 4: write
-	mov ebx, 1             ; Argument 1 int fd: file descriptor. 1 is stdout.
-	mov ecx, INPUT_MSG     ; Argument 2 const char *buf: the string to write
-	mov edx, IN_MSG_LENGTH ; Argument 3 size_t count: number of bytes in but
-	int 0x80               ; int: interrupt. 0x80: run the syscall in eax.
+	; Ask the user to enter a character.
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, INPUT_MSG
+	mov edx, IN_MSG_LENGTH
+	int 0x80
 
+	; Tells the entered character.
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, OUTPUT_MSG
+	mov edx, OUT_MSG_LENGTH
+	int 0x80
+
+	; Exit
 	mov eax, 1 ; Syscall 1: exit
 	mov ebx, 0 ; Return value 0: no error
 	int 0x80
