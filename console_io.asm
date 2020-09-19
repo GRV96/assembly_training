95670f9eb58d4293f648d49e0db51662401cbf3d
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-09-18 21:39:02 -0400

    User's character is read and written in the console.

--- console_io.asm
+++ console_io.asm
@@ -1,13 +1,18 @@
 global _start
 
-.data:
+section .bss
+	a_char resb 1
+
+section .data
 	INPUT_MSG db "Enter a character: "
 	IN_MSG_LENGTH equ $-INPUT_MSG
 
-	OUTPUT_MSG db 0xA, "You have entered "
+	OUTPUT_MSG db "You have entered "
 	OUT_MSG_LENGTH equ $-OUTPUT_MSG
+	OUT_MSG_END db ".", 0xA
+	OUT_MSG_END_LENGTH equ $-OUT_MSG_END
 
-.text:
+section .text
 _start:
 	; Ask the user to enter a character.
 	mov eax, 4
@@ -16,11 +21,30 @@ _start:
 	mov edx, IN_MSG_LENGTH
 	int 0x80
 
-	; Tells the entered character.
+	; Read the character.
+	mov eax, 3
+	mov ebx, 2
+	mov ecx, a_char
+	mov edx, 1
+	int 0x80
+
+	; Tell the entered character.
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, OUTPUT_MSG
 	mov edx, OUT_MSG_LENGTH
+	int 0x80
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, a_char
+	mov edx, 1
+	int 0x80
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, OUT_MSG_END
+	mov edx, OUT_MSG_END_LENGTH
 	int 0x80
 
 	; Exit
